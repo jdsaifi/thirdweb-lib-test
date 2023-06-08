ee1c0f9b7893bd5377191481d905a3f20d95cb66
jD Saifi <jdsaifi729@gmail.com>
2023-06-08 18:37:59 +0530

    first commit

---
+++ _next/static/pMXiIjx09xpUrS8d9T6fX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/794-99486bf632a8288a.js","static/chunks/pages/index-f709421bd0c7c08c.js"],"/_error":["static/chunks/pages/_error-82b79221b9ed784b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
